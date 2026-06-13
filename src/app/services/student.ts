0a7883ef3eea971c1567e3f659352a56f2baa791
CosovanuGabi912 <marius.cosovanu@stud.ubbcluj.ro>
2026-06-13 03:10:41 +0300

    config: use environment files for API URL

--- src/app/services/student.ts
+++ src/app/services/student.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Student } from '../Model/Student';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class StudentService {
-  private url = 'http://localhost:8080/students';
+  private url = `${environment.apiUrl}/students`;
 
   constructor(private http: HttpClient) {}
 
